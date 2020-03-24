1b3c4bcb8597da4321eda2da07bf67ec043abaea
mordegar_admin <mordegar@inbox.ru>
2020-03-24 20:37:18 +0400

    add 222

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Rea222ct
         </a>
       </header>
     </div>
