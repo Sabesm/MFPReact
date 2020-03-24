9c2b68aeaca0f365b951246dcad1f7144029fcc9
mordegar_admin <mordegar@inbox.ru>
2020-03-24 21:07:46 +0400

    init_for_dev

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Rea222ct
+          Learn Rea22232ct
         </a>
       </header>
     </div>
