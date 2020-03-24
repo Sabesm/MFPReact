19291587cf595c450e9f64e3464878e5f4488d0a
mordegar_admin <mordegar@inbox.ru>
2020-03-24 21:25:50 +0400

    Add lll

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Rea222ct
+          Learn ReaLLLct
         </a>
       </header>
     </div>
