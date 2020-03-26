e9033528b4db6d3796f63160fd95d8d7e681c931
mordegar_admin <mordegar@inbox.ru>
2020-03-27 00:08:20 +0400

    new update 27032020

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ReaLLLct
+          Learn ReaLLLct5
         </a>
       </header>
     </div>
